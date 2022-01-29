ab58940ae839ec2f7fe3e5de81226a3f6452ddb5
MYTHILI <mythiliec180801@gmail.com>
2022-01-29 12:35:10 +0530

    adding file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Header from './Components/Header';
+import home_content from './Components/home_content';
 
 function App() {
   return (
     <div >
-   <Header></Header>
+   <Header>
+     <home_content>
+       
+     </home_content>
+   </Header>
     </div>
   );
 }
---
+++ src/Components/home_content.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function home_content() {
+  return <div></div>;
+}
+export default home_content
